f6dd98cf2c0aed612bfc6086b1f0cc4b3f4eebce
John Martel <jonathan.martel@coglinc.ca>
2022-01-12 13:36:02 -0500

    chore(commits): enforce body as sentences and allow long footers
    This allows URL references in the footer tagged with `See: ` and typical
    git trailers. The _body as sentences_ part is to enforce the optional
    body to read like paragraphs and sentences. I find this makes for an
    easier reading, since this can be desirably verbose at times.
    
    See: https://chris.beams.io/posts/git-commit/

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
+const ERROR = 2;
+const ALWAYS = 'always';
+
 module.exports = {
-  extends: ['@commitlint/config-conventional']
-}
+  defaultIgnores: true,
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  helpUrl: 'https://chris.beams.io/posts/git-commit/',
+  ignores: [(commit) => commit.includes('Signed-off-by: dependabot[bot] <support@github.com>')],
+  parserPreset: {
+    parserOpts: {
+      noteKeywords: ['See', 'Signed-off-by', 'Co-authored-by'],
+    },
+  },
+  rules: {
+    'body-case': [ERROR, ALWAYS, 'sentence-case'],
+    'body-full-stop': [ERROR, ALWAYS],
+    'body-leading-blank': [ERROR, ALWAYS],
+    'body-max-line-length': [ERROR, ALWAYS, 72],
+    'footer-leading-blank': [ERROR, ALWAYS],
+    'footer-max-line-length': [ERROR, ALWAYS, Infinity],
+  },
+};
